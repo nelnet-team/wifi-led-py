82b9e15b4aaf9761aaf36b4d760cd68326af69f2
Lee Nelson <lnelson@nelnet.org>
2020-02-17 20:06:25 -0800

    Move configuration to its own class

--- wifi-led.py
+++ wifi-led.py
@@ -7,13 +7,11 @@ import sys
 import re
 import time
 
-#My lame attempt at creating immutable global variables
-#I hate mutable global variables
-config=("wlan0", "/sbin/wpa_cli", 21, 128, 1000)
-
 class WifiStatus:
-    def __init__(self, interface):
-        self.interface=interface
+    def __init__(self, config):
+        self.interface=config.interface
+        self.cmd=config.cmd
+        self.brightness=config.brightness
         self.statusmap={
             "UNKNOWN": 0,
             "DISCONNECTED": 0,
@@ -28,7 +26,7 @@ class WifiStatus:
             "COMPLETED": 2
         }
     def GetState (self):
-        cmd=config[1]
+        cmd=self.cmd
         completed=subprocess.run([cmd, "-i", self.interface, "status"], capture_output=True)
         status=str(completed.stdout)
         rawstate=re.search("wpa_state=(\w+)", status)
@@ -40,22 +38,22 @@ class WifiStatus:
         return retval
 
 class LED:
-    def __init__(self, gpio, brightness):
-        self.gpio=int(gpio)
+    def __init__(self, config):
+        self.gpio=int(config.gpio)
         self.blink=0
-        self.freq=config[4]
-        self.duty=int(brightness)
+        self.freq=int(config.freq)
+        self.duty=int(config.brightness)
         self.blinkoff=int(self.duty / 6)
         self.pigpio=pigpio.pi()
         self.pigpio.set_PWM_frequency(self.gpio, self.freq)
     def LedOn(self):
-        print("Turn on LED {}".format(gpio), flush=True)
+        print("Turn on LED {}".format(self.gpio), flush=True)
         self.pigpio.set_PWM_dutycycle(self.gpio, self.duty)
     def LedOff(self):
-        print("Turn off LED {}".format(str(gpio)), flush=True)
+        print("Turn off LED {}".format(str(self.gpio)), flush=True)
         self.pigpio.set_PWM_dutycycle(self.gpio, 0)
     def LedBlinkOff(self):
-        print("Turn down LED {}".format(str(gpio)), flush=True)
+        print("Turn down LED {}".format(str(self.gpio)), flush=True)
         self.pigpio.set_PWM_dutycycle(self.gpio, self.blinkoff)
     def LedBlink(self):
         if self.blink==1:
@@ -65,53 +63,57 @@ class LED:
             self.blink=1
             self.LedBlinkOff()
 
+class Config:
+    def __init__(self):
+        self.defaults={
+            "interface": "wlan0",
+            "gpio": 21,
+            "brightness": 128
+        }
+        self.cmd="/sbin/wpa_cli"
+        self.freq=1000
+        self.SetInt()
+        self.SetGPIO()
+        self.SetBrightness()
+    def SetInt (self):
+        interface=self.defaults["interface"]
+        if len(sys.argv) > 1:
+            interface=sys.argv[1]
+            print ("Interface {} from command line".format(interface), flush=True)
+        elif "ENV_INT" in os.environ:
+            interface=os.getenv("ENV_INT")
+            print ("Interface {} from environment".format(interface), flush=True)
+        else:
+            print ("Using default interface {}".format(interface), flush=True)
+        self.interface=interface
+    def SetGPIO (self):
+        gpio=self.defaults["gpio"]
+        if len(sys.argv) > 2:
+            gpio=sys.argv[2]
+            print ("GPIO {} from command line".format(gpio), flush=True)
+        elif "ENV_GPIO" in os.environ:
+            gpio=os.getenv("ENV_GPIO")
+            print ("GPIO {} from environment".format(gpio), flush=True)
+        else:
+            print ("Using default GPIO {}".format(gpio), flush=True)
+        self.gpio=gpio
+    def SetBrightness (self):
+        brightness=self.defaults["brightness"]
+        if len(sys.argv) > 3:
+            gpio=sys.argv[3]
+            print ("Brightness {} from command line".format(brightness), flush=True)
+        elif "ENV_BRIGHTNESS" in os.environ:
+            gpio=os.getenv("ENV_BRIFHTNESS")
+            print ("Brightness {} from environment".format(brightness), flush=True)
+        else:
+            print ("Using default brightness {}".format(brightness), flush=True)
+        self.brightness=brightness
 
 
-def GetInt ():
-    interface=config[0]
-    if len(sys.argv) > 1:
-        interface=sys.argv[1]
-        print ("Interface {} from command line".format(interface), flush=True)
-    elif "ENV_INT" in os.environ:
-        interface=os.getenv("ENV_INT")
-        print ("Interface {} from environment".format(interface), flush=True)
-    else:
-        print ("Using default interface {}".format(interface), flush=True)
-    return interface
+config=Config()
+wifi=WifiStatus(config)
+LED=LED(config)
 
-
-def GetGPIO ():
-    gpio=config[2]
-    if len(sys.argv) > 2:
-        gpio=sys.argv[2]
-        print ("GPIO {} from command line".format(gpio), flush=True)
-    elif "ENV_GPIO" in os.environ:
-        gpio=os.getenv("ENV_GPIO")
-        print ("GPIO {} from environment".format(gpio), flush=True)
-    else:
-        print ("Using default GPIO {}".format(gpio), flush=True)
-    return gpio
-
-def GetBrightness ():
-    brightness=config[3]
-    if len(sys.argv) > 3:
-        gpio=sys.argv[3]
-        print ("Brightness {} from command line".format(brightness), flush=True)
-    elif "ENV_BRIGHTNESS" in os.environ:
-        gpio=os.getenv("ENV_BRIFHTNESS")
-        print ("Brightness {} from environment".format(brightness), flush=True)
-    else:
-        print ("Using default brightness {}".format(brightness), flush=True)
-    return brightness
-
-
-interface=GetInt()
-gpio=GetGPIO()
-brightness=GetBrightness()
-
-wifi=WifiStatus(interface)
-
-LED=LED(gpio,brightness)
 oldstate=3
 
 while True:
